2e37e7a8a9703c81fdb26c1960237ae50c43dffa
Sayem Mohammad Siam <sayemsiam@yahoo.com>
2013-05-11 15:25:43 -0700

    Another commit

---
+++ TestOverride/src/BaseClass.java
@@ -0,0 +1,10 @@
+
+public class BaseClass {
+	public BaseClass(){
+		this.foo();
+	}
+
+	private void foo(){
+		System.out.println("in Base foo");
+	}
+}
---
+++ TestOverride/src/DerivedClass.java
@@ -0,0 +1,10 @@
+
+public class DerivedClass extends BaseClass {
+	public static void main(String []args){
+		new DerivedClass();
+	}
+	public void foo(){
+		System.out.println("in Derived foo");
+	}
+
+}
